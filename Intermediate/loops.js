a0eb681b353cdfe4d5e94e68a3cccb8225542835
p-soham11 <sohampal1108@gmail.com>
2020-10-19 17:39:25 +0530

    Adding loops

--- Intermediate/loops.js
+++ Intermediate/loops.js
@@ -15,3 +15,31 @@ for(let i = 0; i < cities.length; i++) {
         continue;           // It will take the loop to next itteration without executing the following statements
     console.log(cities[i]);
 }
+console.log();
+
+
+// Using While Loop
+console.log("Using While Loops : ")
+let i = 0;
+while(i < cities.length) {
+    if ( typeof cities[i] === "string") {
+        console.log(cities[i]);
+    }
+    i++;     // Manually incrementing the counter variable
+} 
+console.log();
+
+
+
+// Using DO-While Loops
+
+// This will execute the code atleast once even if the condition is unsatisfied
+// It's beacuse it execute the code before checking the condition
+console.log("Using Do-While Loops : ")
+let i=0;
+do{
+    if ( typeof cities[i] === "string") {
+        console.log(cities[i]);
+    }
+    i++;    // Incrementing the counter variable   
+} while(i < cities.length);
